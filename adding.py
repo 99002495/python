707d281e8ab2fe5ab86c5ee3771054ff9737126c
Prajwal rao p <71481375+99002495@users.noreply.github.com>
2020-10-15 17:44:11 +0530

    Update adding.py

--- adding.py
+++ adding.py
@@ -1,4 +1,13 @@
+def test_something_that_involves_user_input(monkeypatch):
 
+    # monkeypatch the "input" function, so that it returns "Mark".
+    # This simulates the user entering "Mark" in the terminal:
+    monkeypatch.setattr('builtins.input', lambda _: "Mark")
+
+    # go about using input() like you normally would:
+    i = input("What is your name?")
+    assert i == "Mark"
+    
 def add(a, b):
     if a > 0 and b > 0:
         x=input()
